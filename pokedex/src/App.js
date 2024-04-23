d4dabc38c7bcd81f3183142f46fff4bba217cf44
Gabriela <gabibogdanp008@gmail.com>
2024-04-23 18:37:32 +0200

    configured my project

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -7,7 +7,7 @@ import PokemonDetails from './components/PokemonDetails';
 function App() {
     return (
         <Routes>
-            <Route path="/" element={<Pokedex />} />
+            <Route path="/my_pokedex" element={<Pokedex />} />
             <Route path="/pokemon/:id" element={<PokemonDetails />} />
         </Routes>
     );
